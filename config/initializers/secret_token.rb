e8b7f24aac2e04509a1a6028a93c1a8788098545
root <root@redhat5vm.vmdomain.com>
2011-07-31 12:54:29 -0400

    committed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'b40ee512ffdd8fb44a0a3e6279ff5a77b66f8ed83a4b22108b358b65203951fc90371644c97f1c3e0689ece6e1d45acf8d4683652730eed43e50d426d7cac8f2'
